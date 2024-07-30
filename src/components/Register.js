07aae2cb4ddd2926c9514e3bf1852140526181b0
Jury D'Ambros <jury.dambros@inflyter.com>
2024-07-30 17:04:01 +0200

    add router

---
+++ src/components/Register.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
---
+++ src/components/Router.js
@@ -0,0 +1,18 @@
+export default function ErrorPage() {
+    return (
+        <>
+            <div id='menu'>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Homepage</a>
+                        </li>
+                        <li>
+                            <a href="/register">Register</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </>
+    )
+}
